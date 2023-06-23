6dce5de2b337da57a4011f1d6625fdf681c48d62
ibrezm <ibrezmm@ibrezms-MacBook-Air.local>
2023-06-24 01:23:26 +0530

    initial

--- utils/gcsStorage.js
+++ utils/gcsStorage.js
@@ -6,21 +6,24 @@ class GCSStorage {
   }
 
   async getLast5UploadedItems(bucketName, location) {
+    this.bucketName = bucketName;
+    this.location=location;
     const bucket = this.storage.bucket(bucketName);
     const [files] = await bucket.getFiles({
       autoPaginate: false,
-      directory: location,
+      directory: decodeURI(location),
     });
 
     const sortedFiles = files.sort((a, b) => {
       return b.metadata.timeCreated - a.metadata.timeCreated;
     });
-
+    //https://storage.cloud.google.com/plated-ensign-390102-ml/models/multiclass_text_classification_model.h5
     const last5Files = sortedFiles.slice(0, 5);
-
+    const prefixUrl = `https://storage.cloud.google.com/${bucketName}`;
     return last5Files.map(file => ({
       name: file.name,
       time_created: file.metadata.timeCreated,
+      link: `${prefixUrl}/${file.name}`,
     }));
   }
 }
